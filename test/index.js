2de2142875460df20291076e189ccbc98457df7d
buihuuhai318 <buihuuhai318@gmail.com>
2023-05-05 20:48:53 +0700

    new

---
+++ test/index.js
@@ -0,0 +1,18 @@
+var intro = "hello";
+document.getElementById("intro").innerHTML = intro;
+
+function showMessage() {
+    alert('xin chao');
+}
+
+function showDiv() {
+    document.getElementById("showDiv").innerHTML = "xin chao 2";
+}
+
+function show3() {
+    document.write('xin chao3');
+}
+
+function  show4() {
+    console.log('xin chao 4');
+}
