46c9e6b83a9a4dc97f87d0af13aa647b2ba0b2dc
buihuuhai318 <buihuuhai318@gmail.com>
2023-05-05 21:41:13 +0700

    new

---
+++ practice/toantu.js
@@ -0,0 +1,7 @@
+inputWidth = prompt("enter this width");
+inputHeight = prompt("enter thus height");
+
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("are =  " + area)
